6c0259a070a9eaee814b9ddb7128e9f9d216deee
Ugege Daniel <ugege62@gmail.com>
2024-03-05 23:31:49 +0100

    created basic aut form

--- client/src/components/header/header.component.ts
+++ client/src/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isVisible = false;
+  isConfirmLoading = false;
+  form: FormGroup | undefined;
+  switchValue = false;
 
+  // validateForm: FormGroup<{
+  //   userName: FormControl<string>;
+  //   password: FormControl<string>;
+  //   remember: FormControl<boolean>;
+  // }> = this.fb.group({
+  //   userName: ['', [Validators.required]],
+  //   password: ['', [Validators.required]],
+  //   remember: [true]
+  // });
+  validateForm: FormGroup<{
+    email: FormControl<string>;
+    password: FormControl<string>;
+    remember: FormControl<boolean>;
+  }> = this.fb.group({
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    remember: [true]
+  });
+
+  constructor(private fb: NonNullableFormBuilder) {}
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      remember: [false] // Initial value
+    });
+  }
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    this.isConfirmLoading = true;
+    setTimeout(() => {
+      this.isVisible = false;
+      this.isConfirmLoading = false;
+    }, 1000);
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
 }
---
+++ client/src/components/modal/modal.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent {
+  @Input() isVisible = false;
+  isConfirmLoading = false;
+
+  constructor() {}
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    this.isConfirmLoading = true;
+    setTimeout(() => {
+      this.isVisible = false;
+      this.isConfirmLoading = false;
+    }, 1000);
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+}
