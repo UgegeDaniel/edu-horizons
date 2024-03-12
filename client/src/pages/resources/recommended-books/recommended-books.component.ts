8cfa0f819e83cc62f704aad794578bc05370c987
Ugege Daniel <ugege62@gmail.com>
2024-03-12 08:07:23 +0100

    restructured recommended books page

--- client/src/pages/resources/recommended-books/recommended-books.component.ts
+++ client/src/pages/resources/recommended-books/recommended-books.component.ts
@@ -1,50 +1,107 @@
-import { Component } from '@angular/core';
+import { CollectionViewer, DataSource } from '@angular/cdk/collections';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
+import { catchError, takeUntil } from 'rxjs/operators';
+
+import { NzMessageService } from 'ng-zorro-antd/message';
 interface ItemData {
-  href: string;
-  title: string;
-  avatar: string;
-  description: string;
-  content: string;
+  gender: string;
+  name: Name;
+  email: string;
 }
+
+interface Name {
+  title: string;
+  first: string;
+  last: string;
+}
+
 @Component({
   selector: 'app-recommended-books',
   templateUrl: './recommended-books.component.html',
   styleUrls: ['./recommended-books.component.css']
 })
-export class RecommendedBooksComponent {
-  data: ItemData[] = [];
-  books = [
-    { 
-      title: 'Book 1', 
-      author: 'Author 1', 
-      class: 'Class 1', 
-      subject: 'Subject 1', 
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
-    },
-    { 
-      title: 'Book 2', 
-      author: 'Author 2', 
-      class: 'Class 2', 
-      subject: 'Subject 2', 
-      description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' 
-    },
-    // Add more books as needed
-  ];
+export class RecommendedBooksComponent implements OnInit, OnDestroy{
+  ds = new MyDataSource(this.http);
 
-
+  private destroy$ = new Subject<boolean>();
+  constructor(
+    private http: HttpClient,
+    private nzMessage: NzMessageService
+  ) {}
   ngOnInit(): void {
-    this.loadData(1);
+    this.ds
+      .completed()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.nzMessage.warning('Infinite List loaded all');
+      });
   }
 
-  loadData(pi: number): void {
-    this.data = new Array(5).fill({}).map((_, index) => ({
-      href: 'http://ant.design',
-      title: `ant design part ${index} (page: ${pi})`,
-      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-      description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-      content:
-        'We supply a series of design principles, practical patterns and high quality design resources ' +
-        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.'
-    }));
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
+}
+
+class MyDataSource extends DataSource<ItemData> {
+  private pageSize = 10;
+  private cachedData: ItemData[] = [];
+  private fetchedPages = new Set<number>();
+  private dataStream = new BehaviorSubject<ItemData[]>(this.cachedData);
+  private complete$ = new Subject<void>();
+  private disconnect$ = new Subject<void>();
+
+  constructor(private http: HttpClient) {
+    super();
+  }
+
+  completed(): Observable<void> {
+    return this.complete$.asObservable();
+  }
+
+  connect(collectionViewer: CollectionViewer): Observable<ItemData[]> {
+    this.setup(collectionViewer);
+    return this.dataStream;
+  }
+
+  disconnect(): void {
+    this.disconnect$.next();
+    this.disconnect$.complete();
+  }
+
+  private setup(collectionViewer: CollectionViewer): void {
+    this.fetchPage(0);
+    collectionViewer.viewChange.pipe(takeUntil(this.complete$), takeUntil(this.disconnect$)).subscribe(range => {
+      if (this.cachedData.length >= 50) {
+        this.complete$.next();
+        this.complete$.complete();
+      } else {
+        const endPage = this.getPageForIndex(range.end);
+        this.fetchPage(endPage + 1);
+      }
+    });
+  }
+
+  private getPageForIndex(index: number): number {
+    return Math.floor(index / this.pageSize);
+  }
+
+  private fetchPage(page: number): void {
+    if (this.fetchedPages.has(page)) {
+      return;
+    }
+    this.fetchedPages.add(page);
+
+    this.http
+      .get<{ results: ItemData[] }>(
+        `https://randomuser.me/api/?results=${this.pageSize}&inc=name,gender,email,nat&noinfo`
+      )
+      .pipe(catchError(() => of({ results: [] })))
+      .subscribe(res => {
+        this.cachedData.splice(page * this.pageSize, this.pageSize, ...res.results);
+        this.dataStream.next(this.cachedData);
+      });
   }
 }
